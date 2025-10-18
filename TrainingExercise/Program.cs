b39d00cd37f23deaca2070a9419c96ffd42914e1
d41xie <d4xm@proton.me>
2025-10-18 23:18:00 +0200

    added acumulator app

---
+++ TrainingExercise/Program.cs
@@ -0,0 +1,21 @@
+﻿// TrainingExercise by Daniil
+
+Console.WriteLine("Welcome to the Accumulator!");
+Console.WriteLine("Enter a negative number to quit:");
+
+int newNumber = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+while(newNumber >= 0)
+{
+    if (newNumber >= 0)
+    {
+        sum += newNumber;
+        Console.WriteLine("Sum: " + sum);
+        Console.WriteLine("Enter a negative number to quit:");
+        newNumber = Convert.ToInt32(Console.ReadLine());
+    }
+    
+}
+    Console.WriteLine("Final Sum: " + sum);
+    Console.WriteLine("Goodbye!");
